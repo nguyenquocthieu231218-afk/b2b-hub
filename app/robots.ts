e58a6952c0ae99e2f8927f3e063d0c93c582bc4b
nguyenquocthieu231218-afk <nguyenquocthieu231218@gmail.com>
2026-09-08 13:05:29 +0700

    Add robots.ts for metadata route configuration

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://b2b-hub-orpin.vercel.app/sitemap.xml',
+  };
+}
