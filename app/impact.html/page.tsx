1b8b6ec032a46acd51ec07126a796a3197a7491e
nguyenquocthieu231218-afk <nguyenquocthieu231218@gmail.com>
2026-09-24 19:19:14 +0700

    Create ImpactVerificationPage component

---
+++ app/impact.html/page.tsx
@@ -0,0 +1,13 @@
+export default function ImpactVerificationPage() {
+  return (
+    <html>
+      <head>
+        <title>Impact Verification</title>
+        <meta name="impact-site-verification" value="8b40ba0e-e24c-4340-bb46-5f63f9772766" />
+      </head>
+      <body>
+        8b40ba0e-e24c-4340-bb46-5f63f9772766
+      </body>
+    </html>
+  );
+}
